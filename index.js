dfe4827be932b99e073da37e0784ec3db7a18cad
Zacharyjg <guldb016@umn.edu>
2021-07-10 21:00:45 -0500

    Added the construction and writing of the html file

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const Intern = require("./lib/Intern");
 
 let team = [];
 
+const htmlBeginning = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <title>Team Profile</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-primary justify-content-center">
+        <h1>My Team</h1>
+    </nav>
+
+    <div class="row justify-content-around">
+`;
+
+const htmlEnding = `</div>
+</body>
+
+</html>`;
 const addManager = () => {
     inquirer.prompt([
         {
@@ -32,6 +55,7 @@ const addManager = () => {
         const newManager = new Manager(name, id, email, officeNumber);
 
         team.push(newManager);
+        console.log("Successfully added Manager!");
         start();
     });
 }
@@ -62,7 +86,7 @@ const addEngineer = () => {
         const newEngineer = new Engineer(name, id, email, github);
 
         team.push(newEngineer);
-
+        console.log("Successfully added Engineer!");
         start();
     });
 }
@@ -93,10 +117,69 @@ const addIntern = () => {
         const newIntern = new Intern(name, id, email, school);
 
         team.push(newIntern);
+        console.log("Successfully added Intern!");
 
         start();
     });
 }
+
+const renderHTML = () => {
+    let htmlContent = "";
+    htmlContent += htmlBeginning;
+    team.forEach(member => {
+        if (member.getRole() == "Manager") {
+            htmlContent += 
+        `       <div class="card col-2.5">
+            <h4 class="card-header">${member.getName()} - ${member.getRole()}</h4>
+            <div class="card-body">
+                <h5>ID: ${member.getId()}</h5>
+                <h5>Email: <a href="mailto:${member.getEmail()}">
+                        ${member.getEmail()}
+                    </a></h5>
+                <h5>Office Number: ${member.getOffice()}</h5>
+            </div>
+        </div>
+        `;
+        } else if (member.getRole() == "Engineer") {
+            htmlContent += 
+        `       <div class="card col-2.5">
+            <h4 class="card-header">${member.getName()} - ${member.getRole()}</h4>
+            <div class="card-body">
+                <h5>ID: ${member.getId()}</h5>
+                <h5>Email: <a href="mailto:${member.getEmail()}">
+                        ${member.getEmail()}
+                    </a></h5>
+                <h5>GitHub: <a href="https://github.com/${member.getGithub()}" target="_blank">
+                        ${member.getGithub()}
+                    </a></h5>
+            </div>
+        </div>
+        `;
+        } else if (member.getRole() == "Intern") {
+            htmlContent += 
+        `       <div class="card col-2.5">
+            <h4 class="card-header">${member.getName()} - ${member.getRole()}</h4>
+            <div class="card-body">
+                <h5>ID: ${member.getId()}</h5>
+                <h5>Email: <a href="mailto:${member.getEmail()}">
+                        ${member.getEmail()}
+                    </a></h5>
+                <h5>School: ${member.getSchool()}</h5>
+            </div>
+        </div>
+        `;
+        }
+    });
+
+    htmlContent += htmlEnding;
+    fs.writeFile("index.html", htmlContent, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
 
 const start = () => {
     inquirer.prompt([
@@ -117,7 +200,7 @@ const start = () => {
                 break;
 
             case "Finish building team":
-                console.log(team);
+                renderHTML();   
                 console.log("Team successfully built! Please check HTML file for generated team webpage.")
                 break;
         }
